1faf4e776b857884390274d0ad23da8417770814
Facundo Garcia <facundo.garcia@ibisdev.tech>
2021-11-05 19:42:03 -0300

    variables de entorno

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(
+    "mongodb+srv://facundo-portfolio:Artaud1973@fletech.o6psu.mongodb.net/portfolio?retryWrites=true w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
   .then(() => console.log("Connected to database."))
   .catch(() => {
     console.log("Cannot connect to database. Exiting.");
