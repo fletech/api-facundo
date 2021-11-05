e94214510058fbf2e04c5f1f0e4917c191159360
Facundo Garcia <facundo.garcia@ibisdev.tech>
2021-11-05 19:14:28 -0300

    first commit

---
+++ api/controllers/api_portfolioController.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+const Post = require("../database/portfolio/models/post");
+const Message = require("../database/portfolio/models/message");
+const User = require("../database/portfolio/models/user");
+
+module.exports = {
+  //BLOG
+  allBlogPosts: (req, res) => {
+    Post.find()
+      .exec()
+      .then((result) => res.status(200).json(result))
+      .catch((err) => res.status(500).json({ error: err }));
+  },
+  oneBlogPosts: (req, res) => {
+    const id = req.params.id;
+    Post.findById(id)
+      .exec()
+      .then((doc) => res.status(200).json(doc))
+      .catch((err) => res.status(500).json({ error: err }));
+    //res.status(200).json({});
+  },
+  newBlogPost: (req, res) => {
+    const post = new Post({
+      _id: new mongoose.Types.ObjectId(),
+      title: req.body.title,
+      body: req.body.body,
+      author: req.body.author,
+    });
+
+    post
+      .save()
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+
+    return res.send(product);
+  },
+  editBlogPost: (req, res) => {},
+  deleteBlogPost: (req, res) => {},
+
+  //MESSAGES
+  allMessages: (req, res) => {
+    Message.find()
+      .exec()
+      .then((result) => res.status(200).json(result))
+      .catch((err) => res.status(500).json({ error: err }));
+  },
+  oneMessage: (req, res) => {
+    const id = req.params.id;
+    Message.findById(id)
+      .exec()
+      .then((doc) => res.status(200).json(doc))
+      .catch((err) => res.status(500).json({ error: err }));
+  },
+  newMessage: (req, res) => {
+    const message = new Message({
+      _id: new mongoose.Types.ObjectId(),
+      subject: req.body.subject,
+      message: req.body.message,
+      name: req.body.name,
+      email: req.body.email,
+    });
+
+    message
+      .save()
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  readMessage: (req, res) => {},
+  deleteMessage: (req, res) => {
+    const _id = req.params.id;
+    Message.findByIdAndRemove(_id)
+      .exec()
+      .then((doc) => res.status(204).json(doc))
+      .catch((err) => res.status(500).json({ error: err }));
+  },
+  //LOGIN ALKEMY
+  loginAlkemy: (req, res) => {
+    if (
+      req.body.email === "challenge@superhero.com" &&
+      req.body.password === "react&&challenge"
+    ) {
+      res.status(200).json({
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJjaGFsbGVuZ2VAYWxrZW15Lm9yZyIsImlhdCI6MTUxNjIzOTAyMn0.ilhFPrG0y7olRHifbjvcMOlH7q2YwlegT0f4aSbryBE",
+      });
+    } else {
+      res.status(400).json({ error: "not allowed" });
+    }
+  },
+};
---
+++ api/routes/api_portfolio.js
@@ -0,0 +1,37 @@
+let express = require("express");
+const path = require("path");
+// let multer = require("multer");
+// let mime = require("mime");
+
+let router = express.Router();
+let apiPortfolioController = require("../controllers/api_portfolioController");
+
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, "../public/images/uploads"));
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(file);
+//     cb(null, file.originalname + "." + mime.getExtension(file.mimetype));
+//   },
+// });
+// var upload = multer({ storage: storage });
+
+/* BLOG */
+router.get("/posts", apiPortfolioController.allBlogPosts);
+router.get("/post/:id", apiPortfolioController.oneBlogPosts);
+router.post("/new-post", apiPortfolioController.newBlogPost);
+router.patch("/post/:id", apiPortfolioController.editBlogPost);
+router.delete("/post/:id", apiPortfolioController.deleteBlogPost);
+
+/*CONTACT*/
+router.get("/messages", apiPortfolioController.allMessages);
+router.get("/messages/:id", apiPortfolioController.oneMessage);
+router.post("/new-message", apiPortfolioController.newMessage);
+router.patch("/messages/:id", apiPortfolioController.readMessage);
+router.delete("/messages/:id", apiPortfolioController.deleteMessage);
+
+/*USERS*/
+
+router.post("/users", apiPortfolioController.loginAlkemy);
+module.exports = router;
